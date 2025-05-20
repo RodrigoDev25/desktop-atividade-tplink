6a53bd1f664f9e23d63241791baa9be670974132
Rodrigo Silva <rodrigo.codeshub25@gmail.com>
2025-05-20 20:21:54 -0300

    Arrumando .js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,20 +1,37 @@
 let submitVar = document.getElementById("button-submit")
-    
+
 submitVar.addEventListener("click", function () {
+    //Criando definições pré-definidas   
     const user = "admin"
     const password = "1234"
     const code = "tplink2025"
 
+    //Capturando informações dos campos e armazenando em variáveis
     let usuarioVar = document.getElementById("usuario").value
     let senhaVar = document.getElementById("senha").value
     let codigoVar = document.getElementById("codigo").value
 
+    //Verificanse se os campos não estão vazios
+    if (usuarioVar.trim() === "") {
+        alert("Preencha o campo: Usuário");
+        return
+    }
+    if (senhaVar.trim() === "") {
+        alert("Preencha o campo: Senha");
+        return
+    }
+    if (codigoVar.trim() === "") {
+        alert("Preencha o campo: Código de Instalação");
+        return
+    }
+
+    //Verificando se os dados inseridos estão corretos    
     if (usuarioVar == user && senhaVar == password && codigoVar == code) {
+        //Caso dados estiverem certos direciona para o painel        
         window.location.href = ("painel.html")
     }
-
+    //Se os dados não estiverem errados mostra aviso
     else {
-        alert("Ops! Você errou e-mail e senha")
+        alert('Erro!');
     }
-
-})
+});
